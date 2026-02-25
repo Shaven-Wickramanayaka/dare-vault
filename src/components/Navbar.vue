98a4ab08660b558510ff44a5039a04cfa353e546
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-02-25 12:46:15 +0000

    Basic Navbar with working links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,56 @@
-<script>
-import SignOutButton from "./SignOutButton.vue";
-
-export default {
-  name: "Navbar",
-  components: {
-    SignOutButton,
-  },
-};
+<script setup>
+import { computed } from "vue";
+import { useCurrentUser } from "vuefire";
+import SignOutButton from "../components/SignOutButton.vue";
+const user = useCurrentUser();
+const username = computed(() => user.value?.displayName || null);
 </script>
 <template>
-  <nav class="bg-gray-800 text-white w-full">
-    <span> <h1 class="text-left p-4">DareVault</h1> </span
-    ><span>
-      <h1 class="text-right p-4">Log In</h1>
-    </span>
+  <nav
+    class="bg-white w-full flex relative justify-between items-center mx-auto px-8 h-20"
+  >
+    <div class="inline-flex">
+      <Router-Link class="text-xl">Darevault</Router-Link>
+    </div>
+
+    <!-- end logo -->
+    <!-- login -->
+    <div class="flex-initial">
+      <div class="flex justify-end items-center relative">
+        <div class="flex mr-4 items-center">
+          <a
+            class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full"
+            href="#"
+          >
+            <div
+              class="flex items-center relative cursor-pointer whitespace-nowrap"
+            >
+              <div v-if="username" class="">
+                <Router-Link to="/dashboard">Dashboard</Router-Link>
+              </div>
+              <div v-else class="">
+                <Router-Link to="/login">Log In</Router-Link>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="block">
+          <div class="inline relative">
+            <button
+              type="button"
+              class="inline-flex items-center relative px-2 border rounded-full hover:shadow-lg"
+            >
+              <h2 v-if="username">Hello {{ username }}</h2>
+              <h2 v-else>Hello Stranger</h2>
+              <div class="block flex-grow-0 flex-shrink-0 h-10 w-12 pl-5">
+                <SignOutButton></SignOutButton>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end login -->
   </nav>
 </template>
-<style lang="css" scoped></style>
