a4dbbb7b72c4dca68d009311e8ac1ed8fb4fcc88
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-03-08 15:50:40 +0000

    Added PWA scrnshots and removed external manifest file

--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,69 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
-// https://vite.dev/config/
+
 export default defineConfig({
   base: "/",
   plugins: [
     vue(),
     tailwindcss(),
     VitePWA({
-      registerType: "autoUpdate", // Automatically updates the service worker
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "favicon-96x96.png",
+      ],
       manifest: {
-        name: "My Awesome Vue App",
-        short_name: "VueApp",
-        description: "A description of my PWA",
-        theme_color: "#42b883",
+        id: "/",
+        name: "Darevault",
+        short_name: "DareVault",
+        description:
+          "Spend less time thinking of dares and more time embarrassing your friends",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "web-app-manifest-192x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "any",
           },
           {
-            src: "pwa-512x512.png",
+            src: "web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "img/icons/android-chrome-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "img/icons/android-chrome-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        screenshots: [
+          {
+            src: "screenshots/desktop-home.png",
+            sizes: "1920x1080",
+            type: "image/png",
+            form_factor: "wide",
+            label: "Darevault desktop home",
+          },
+          {
+            src: "screenshots/mobile-home.png",
+            sizes: "1080x1920",
+            type: "image/png",
+            label: "Darevault mobile home",
           },
         ],
       },
