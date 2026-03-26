fc1507602ff43f762e7c149f3651e93cba933af5
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-03-26 20:17:33 +0000

    Different ascii heading and how to use section made

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -23,39 +23,149 @@ onMounted(() => {
 </script>
 <template>
   <Navbar></Navbar>
-  <section class="flex flex-col justify-center items-center text-center">
+  <section class="flex flex-col justify-center items-center text-center h-160">
     <pre
-      class="text-(--text-on-dark) text-[0.9rem] md:text-[1.3rem] lg:text-[1.5rem] mt-7"
+      class="text-(--text-on-dark) text-[0.5rem] sm:text-[0.6rem] md:text-[1.4rem] lg:text-[1.5rem] mt-3"
     >
- ____                                  _ _   
-|  _ \  __ _ _ __ _____   ____ _ _   _| | |_ 
-| | | |/ _` | '__/ _ \ \ / / _` | | | | | __|
-| |_| | (_| | | |  __/\ V / (_| | |_| | | |_ 
-|____/ \__,_|_|  \___| \_/ \__,_|\__,_|_|\__|
-                                              
+██████╗  █████╗ ██████╗ ███████╗██╗   ██╗ █████╗ ██╗   ██╗██╗  ████████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝██║   ██║██╔══██╗██║   ██║██║  ╚══██╔══╝
+██║  ██║███████║██████╔╝█████╗  ██║   ██║███████║██║   ██║██║     ██║   
+██║  ██║██╔══██║██╔══██╗██╔══╝  ╚██╗ ██╔╝██╔══██║██║   ██║██║     ██║   
+██████╔╝██║  ██║██║  ██║███████╗ ╚████╔╝ ██║  ██║╚██████╔╝███████╗██║   
+╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝  ╚═══╝  ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝   
+               
      </pre
     >
     <div
-      class="grid grid-cols-2 grid-rows-4 gap-20 bg-(--color-evergreen) h-60 aspect-3/2 md:w-150 lg:w-200 md:aspect-3/1 mt-6 mx-6 p-6 rounded-2xl"
+      class="flex justify-center items-center bg-(--color-evergreen) h-60 aspect-3/2 md:w-150 lg:w-200 md:aspect-3/1 mt-6 mx-6 p-6 rounded-2xl"
     >
-      <div
-        class="h-30 rounded-2xl aspect-square col-span-1 row-span-1 bg-amber-400"
-      ></div>
-      <div
-        class="h-30 m-10 rounded-2xl aspect-square col-span-1 row-span-1 bg-amber-400"
-      ></div>
-      <div
-        class="h-30 m-10 rounded-2xl aspect-square col-span-1 row-span-1 bg-amber-400"
-      ></div>
-      <div
-        class="h-20 m-10 rounded-2xl aspect-square col-span-1 row-span-1 bg-amber-400"
-      ></div>
-      <div
-        class="h-20 m-10 rounded-2xl aspect-square col-span-1 row-span-1 bg-amber-400"
-      ></div>
+      <p class="text-2xl font-[Raleway] font-black text-(--color-mint-bright)">
+        "Stop forgetting that question you want to ask your friend. Use
+        Darevault"
+      </p>
     </div>
   </section>
-  <!-- Do not annotate ascii bottom row, i do not know how to fix the alignment error anyother way -->
+
+  <section class="flex flex-col items-center text-center h-100">
+    <h2 class="text-[2.5rem] font-[Pacifico] text-(--text-on-dark)">
+      How it works
+    </h2>
+    <div class="grid grid-cols-4 grid-rows-4 gap-x-15">
+      <div class="aspect-square flex justify-center flex-col items-center w-40">
+        <h3
+          class="p-2 text-2xl font-[Raleway] text-(--color-sage-dark) font-bold"
+        >
+          Step 1
+        </h3>
+        <img
+          src="https://placehold.co/400"
+          alt=""
+          width="200px"
+          class="rounded-2xl"
+        />
+        <legend
+          class="p-2 font-[Raleway] font-medium text-(--color-mint-bright)"
+        >
+          Sign in using your email or google.
+        </legend>
+      </div>
+      <div class="aspect-square flex justify-center flex-col items-center w-40">
+        <h3
+          class="p-2 text-2xl font-[Raleway] text-(--color-sage-dark) font-bold"
+        >
+          Step 2
+        </h3>
+        <img
+          src="https://placehold.co/400"
+          alt=""
+          width="200px"
+          class="rounded-2xl"
+        />
+        <legend
+          class="p-2 font-[Raleway] font-medium text-(--color-mint-bright)"
+        >
+          Make a new vault.
+        </legend>
+      </div>
+      <div class="aspect-square flex justify-center flex-col items-center w-40">
+        <h3
+          class="p-2 text-2xl font-[Raleway] text-(--color-sage-dark) font-bold"
+        >
+          Step 3
+        </h3>
+        <img
+          src="https://placehold.co/400"
+          alt=""
+          width="200px"
+          class="rounded-2xl"
+        />
+        <legend
+          class="p-2 font-[Raleway] font-medium text-(--color-mint-bright)"
+        >
+          Invite your friends using the vault id and name.
+        </legend>
+      </div>
+      <div class="aspect-square flex justify-center flex-col items-center w-40">
+        <h3
+          class="p-2 text-2xl font-[Raleway] text-(--color-sage-dark) font-bold"
+        >
+          Step 4
+        </h3>
+        <img
+          src="https://placehold.co/400"
+          alt=""
+          width="200px"
+          class="rounded-2xl"
+        />
+        <legend
+          class="p-2 font-[Raleway] font-medium text-(--color-mint-bright)"
+        >
+          Add dares!
+        </legend>
+      </div>
+      <div class="aspect-square flex justify-center flex-col items-center w-40">
+        <h3
+          class="p-2 text-2xl font-[Raleway] text-(--color-sage-dark) font-bold"
+        >
+          Step 5
+        </h3>
+        <img
+          src="https://placehold.co/400"
+          alt=""
+          width="200px"
+          class="rounded-2xl"
+        />
+        <legend
+          class="p-2 font-[Raleway] font-medium text-(--color-mint-bright)"
+        >
+          Start a session
+        </legend>
+      </div>
+      <div class="aspect-square flex justify-center flex-col items-center w-40">
+        <h3
+          class="p-2 text-2xl font-[Raleway] text-(--color-sage-dark) font-bold"
+        >
+          Step 6
+        </h3>
+        <img
+          src="https://placehold.co/400"
+          alt=""
+          width="200px"
+          class="rounded-2xl"
+        />
+        <legend
+          class="p-2 font-[Raleway] font-medium text-(--color-mint-bright)"
+        >
+          Get exposed
+        </legend>
+      </div>
+      <div
+        class="col-span-2 row-span-1 font-[Pacifico] flex justify-center items-center text-5xl aspect-auto text-(--text-on-dark)"
+      >
+        Thats it!
+      </div>
+    </div>
+  </section>
   <!-- <section>
       <div class="w-full text-center">
         <h1 class="text-4xl p-3 font-[Raleway] font-bold my-5 text-[#E83338]">
